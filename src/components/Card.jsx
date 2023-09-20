9b6494244eaca6fa8437af25347ebd1bcd4e9039
Dewald <dewaldfourie0808@gmail.com>
2023-09-20 12:58:08 +0200

    implemented initial game play functionality

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable react/prop-types */
 
-const Card = ({ image, onClick}) => {
+const Card = ({image, onClick}) => {
     
     
     return (
         <div className="card" onClick={() => onClick(image)}>
             <img className="card-image" src={image} alt="Card-Image" />
+
         </div>
     )
 }
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -2,67 +2,78 @@ import { useState, useEffect } from "react";
 import Card from "./Card";
 
 const Game = () => {
-    const [cards, setCards] = useState([]); // to store all card images
+    const [uniqueCards, setUniqueCards] = useState([]); // to store unique card images
     const [selected, setSelected] = useState([]); // store selected images
     const [score, setScore] = useState(0);
+    const [gameOver, setGameOver] = useState(false);
 
-    // function to shuffle an array randomly  (using Fisher-Yates algorithm)
-    const shuffleArray = (array) => {
-        const shuffledArray = [...array];
-        for ( let i = shuffledArray.length - 1; i > 0; i-- ) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [shuffleArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]]
-        }
-        return shuffleArray;
-    }
+    // Function to fetch unique card images from the API
+    const fetchUniqueCards = async () => {
+        try {
+            const response = await fetch("https://api.giphy.com/v1/gifs/search?api_key=Vc1Ljk0gfVQnKl1LE4qhogxyFMKZc6LN&q=golf&limit=12&offset=2&rating=pg&lang=en&bundle=messaging_non_clips");
+            const data = await response.json();
+            const cardImages = data.data.map((data) => data.images.original.url);
+            
+            // Shuffle the fetched cards
+            const shuffledCards = shuffleArray(cardImages);
 
-    // to fetch images from API and store them in cards
-    useEffect(() => {
-        fetch("https://api.giphy.com/v1/gifs/search?api_key=Vc1Ljk0gfVQnKl1LE4qhogxyFMKZc6LN&q=golf&limit=12&offset=2&rating=pg&lang=en&bundle=messaging_non_clips")
-            .then((response) => response.json())
-            .then((data) => {
-                const cardImages = data.data.map((data) => data.images.original.url );
-                console.log("Fetched cardImages:", cardImages);
-                setCards(cardImages)
-            })
-            .catch((error) => {
-                console.error("Error fetching data from Giphy API:", error);
-                setCards([])
-            })
-    }, []);
-
-    useEffect(() => {
-        console.log(cards);
-    }, [cards]);
-
-    const handleCardClick = (image) => {
-        if (selected.includes(image)) {
-            // Game over, reset selected and score
-            setSelected([]);
-            setScore(0);
-            setCards(shuffleArray(cards))
-        } else {
-            // continue the game
-            setSelected([...selected, image]);
-            setScore(score + 1);
-            setCards(shuffleArray(cards))
+            // Set the unique cards state with shuffled cards
+            setUniqueCards(shuffledCards);
+        } catch (error) {
+            console.error("Error fetching data from Giphy API:", error);
+            setUniqueCards([]);
         }
     };
 
+    // Function to shuffle an array randomly (Fisher-Yates algorithm)
+    const shuffleArray = (array) => {
+        const shuffledArray = [...array];
+        for (let i = shuffledArray.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+        }
+        return shuffledArray;
+    };
+
+    useEffect(() => {
+        // Fetch unique cards when the component mounts
+        fetchUniqueCards();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    useEffect(() => {
+        console.log(score);
+    }, [score])
+
+    const handleCardClick = (image) => {
+        if (selected.includes(image)) {
+            setGameOver(true)
+            // Game over, reset selected and score
+
+        } else {
+            // Continue the game
+            setSelected([...selected, image]);
+            setScore(score + 1);
+        }
+    
+        // Shuffle the unique cards array and update the state
+        const shuffledUniqueCards = shuffleArray([...uniqueCards]);
+        setUniqueCards(shuffledUniqueCards);
+    };
+    
     return (
         <div className="game">
             <h1>Memory Game</h1>
-            <p>Score: {score}</p>
+            <p>{gameOver ? (`GAME OVER , SCORE: ${score}`) : (`Score: ${score}`)}</p>
             <div className="grid-container">
                 <div className="card-container">
-                    {cards.map((image, index) => (
-                        <Card key={index} image={image} onClick={handleCardClick} />
+                    {uniqueCards.map((image, index) => (
+                        <Card key={index} image={image} onClick={gameOver ? () => {} : handleCardClick} />
                     ))}
                 </div>
             </div>
         </div>
-    )
-
-}
+    );
+};
 
 export default Game;
